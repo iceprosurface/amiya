e858e8dbb29373941bf88329ad038ce313ad7a68
iceprosurface <iceprosurface@gmail.com>
2026-02-01 23:00:34 +0800

    chore(global): 新增代理环境变量配置
    在生产环境配置中添加 http_proxy、https_proxy、all_proxy 和 no_proxy 环境变量

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -10,6 +10,10 @@ module.exports = {
       watch: false,
       env: {
         NODE_ENV: "production",
+        http_proxy: "",
+        https_proxy: "",
+        all_proxy: "",
+        no_proxy: "localhost,127.0.0.1,.feishu.cn,.larksuite.com,.larkoffice.com",
       },
     },
   ],
